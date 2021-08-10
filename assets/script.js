eb017bf8378e4fef08083c88edbb402c21c70676
Zachary Dowd <zdowd2796@gmail.com>
2021-08-09 19:36:23 -0600

    functioning quiz pre highscores

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var timeEl = document.querySelector("#time");
 var startButtonEl = document.querySelector("#start");
-var questionsEl = document.querySelector(".questions")
+var questionsEl = document.querySelectorAll(".questions")
 var quest1El = document.querySelector("#q1");
 var quest2El = document.querySelector("#q2");
 var quest3El = document.querySelector("#q3");
@@ -11,17 +11,14 @@ var quest7El = document.querySelector("#q7");
 var endEl = document.querySelector("#endscreen");
 var winlossEl = document.querySelector("#winlossdisplay");
 var scoresEl = document.querySelector("#scoresec");
+var resetEl = document.querySelector('#reset')
 var highscorseEl = document.querySelector("#highscores");
 var startSecEl = document.querySelector("#startsec");
-var correctEl = document.querySelector("#correct");
-var wrongEl = document.querySelector(".wrong")
-// var correct2El = document.querySelector("#correct2");
-// var correct3El = document.getElementById("#correct3");
-// var correct4El = document.getElementById("#correct4");
-// var correct5El = document.getElementById("#correct5");
-// var correct6El = document.getElementById("#correct6");
-// var correct7El = document.getElementById("#correct7");
-var nextEl = document.querySelector("#next");
+var correctEl = document.querySelectorAll("#correct");
+var wrong1El = document.querySelectorAll(".wrong1");
+var wrong2El = document.querySelectorAll(".wrong2");
+var wrong3El = document.querySelectorAll(".wrong3");
+var nextEl = document.querySelectorAll("#next");
 var timeLeft = 60;
 
 startButtonEl.addEventListener("click", start)
@@ -44,13 +41,11 @@ hideAll();
 function start(){
     startButtonEl.disabled = true;
     timer();
-    questions()
-    loghighscores();
 }
-
+var timeInterval;
 //Starts and ends timer
 function timer(){
-    var timeInterval = setInterval(function(){
+    timeInterval = setInterval(function(){
         if (timeLeft > 0) {
             timeEl.textContent = "Seconds Remaining: " + timeLeft;
             timeLeft--;
@@ -62,55 +57,97 @@ function timer(){
             endEl.hidden = false;          
         }
     }, 1000)
+    startSecEl.hidden = true;
     quest1El.hidden = false;
 };
 
 //wrong answer function
-function wrong(){
+function wrong(index){
     var wrongAnswer = document.createElement("p");
     wrongAnswer.textContent = "Wrong!";
-    questionsEl.appendChild(wrongAnswer);
+    questionsEl[index].appendChild(wrongAnswer);
     timeLeft = timeLeft - 10;
-    wrongEl.disabled = true;
-    nextEl.disabled = false;
+    correctEl[index].disabled = true;
+    wrong1El[index].disabled = true;
+    wrong2El[index].disabled = true;
+    wrong3El[index].disabled = true;
+    nextEl[index].disabled = false;
 }
-wrongEl.addEventListener("click", wrong);
+
+wrong1El.forEach(function(el, index){
+    el.addEventListener("click", function(){
+        wrong(index);
+    })
+});
+wrong2El.forEach(function(el, index){
+    el.addEventListener("click", function(){
+        wrong(index);
+    })
+});
+wrong3El.forEach(function(el, index){
+    el.addEventListener("click",  function(){
+        wrong(index);
+    })
+});
+// wrong1El.addEventListener("click", wrong);
+// wrong2El.addEventListener("click", wrong);
+// wrong3El.addEventListener("click", wrong);
 
 //correct answer function
-function correct(){
+function correct(index){
     var corAnswer = document.createElement("p");
     corAnswer.textContent = "Correct!";
-    questionsEl.appendChild(corAnswer);
-    correctEl.disabled = true;
-    nextEl.disabled = false;
+    questionsEl[index].appendChild(corAnswer);
+    correctEl[index].disabled = true;
+    wrong1El[index].disabled = true;
+    wrong2El[index].disabled = true;
+    wrong3El[index].disabled = true;
+    nextEl[index].disabled = false;
 }
-correctEl.addEventListener("click", correct);
 
-// //next answer functon
-// function next() {
-//     if(nextEl.disabled === false && quest1El.hidden === false){
-//         quest2El.hidden = false;
-//         quest1El.hidden = true;
-//     } else if(nextEl.disabled === false && quest2El.hidden === false){
-//         quest3El.hidden = false;
-//         quest2El.hidden = true;
-//     }else if(nextEl.disabled === false && quest3El.hidden === false){
-//         quest4El.hidden = false;
-//         quest3El.hidden = true;
-//     } else if(nextEl.disabled === false && quest4El.hidden === false){
-//         quest5El.hidden = false;
-//         quest4El.hidden = true;
-//     }else if(nextEl.disabled === false && quest5El.hidden === false){
-//         quest6El.hidden = false;
-//         quest5El.hidden = true;
-//     }else if(nextEl.disabled === false && quest6El.hidden === false){
-//         quest7El.hidden = false;
-//         quest6El.hidden = true;
-//     }else if(nextEl.disabled === false && quest7El.hidden === false){
-//         endEl.hidden = false;
-//         quest7El.hidden = true;
-//     }  
-//     wrong();
-//     correct();
-// }
-// nextEl.addEventListener("click", next)
+correctEl.forEach(function(el, index){
+    el.addEventListener("click", function(){
+        correct(index);
+    });
+})
+
+//next answer functon
+function next(index) {
+    var notDisabled = nextEl[index].disabled === false 
+    if(notDisabled && quest1El.hidden === false){
+        quest2El.hidden = false;
+        quest1El.hidden = true;
+    } else if(notDisabled && quest2El.hidden === false){
+        quest3El.hidden = false;
+        quest2El.hidden = true;
+    }else if(notDisabled && quest3El.hidden === false){
+        quest4El.hidden = false;
+        quest3El.hidden = true;
+    } else if(notDisabled && quest4El.hidden === false){
+        quest5El.hidden = false;
+        quest4El.hidden = true;
+    }else if(notDisabled && quest5El.hidden === false){
+        quest6El.hidden = false;
+        quest5El.hidden = true;
+    }else if(notDisabled && quest6El.hidden === false){
+        quest7El.hidden = false;
+        quest6El.hidden = true;
+    }else if(notDisabled && quest7El.hidden === false){
+        endEl.hidden = false;
+        quest7El.hidden = true;
+        clearInterval(timeInterval);
+    } 
+}
+
+nextEl.forEach(function(el, index){
+    el.addEventListener("click", function(){
+        next(index);
+    })
+})
+
+
+//reset function
+function resetGame(){
+    window.location.reload();
+}
+resetEl.addEventListener("click", resetGame)
